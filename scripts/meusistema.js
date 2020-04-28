35857132eab9431d88ddb05bc97bd581ccaba666
Paulo Filipe <63798027+Paulo-Filipe@users.noreply.github.com>
2020-04-28 13:22:27 -0300

    Script att(tables)
    Atualizei o scrpt de geração de tabelas. Entradas até 20 caracteres e impede cadastros duplicados.

--- scripts/meusistema.js
+++ scripts/meusistema.js
@@ -1,28 +1,46 @@
 userID = 1
 bookID = 1
-
-
+var userlist = []
+var booklist = []
 
 function addName(){
 	var val = document.querySelector('#cadastrarNome').value;
-	if((val !== undefined) && (val!== "Nome Completo")){
-		if(val.lenght >== 28){
-			alert("This name is too long!");
-		}
-		var tr = document.querySelector("#userTable");
-		tr.innerHTML += "<tr>\n<td>" + userID + "</td>\n<td>" + val + "</td>\n</tr>";
-		userID += 1;
+	if (val.length >= 20){
+		alert("This name is too long!");
+		document.querySelector('#cadastrarNome').value = "Nome Completo";
+	} else if (userlist.includes(val)) {
+		alert("This name is already on the system!");
+		document.querySelector('#cadastrarNome').value = "Nome Completo";
+	} else if(val !== undefined){
+
+			var tr = document.querySelector("#userTable");
+			tr.innerHTML += "<tr>\n<td>" + userID + "</td>\n<td>" + val + "</td>\n</tr>";
+			userID += 1;
+			userlist.push(val);
+			document.querySelector('#cadastrarNome').value = "Nome Completo";
 	}
 }
+
 function addBook(){
 	var val = document.querySelector('#cadastrarLivro').value;
-	if((val !== undefined) && (val!== "Livro")){
-		if(val.lenght >== 28){
-			alert("The book name is too long!");
-		}
+	if (val.length >= 20){
+		alert("This book name is too long!");
+		val="Livro";
+	}else if (booklist.includes(val)) {
+		alert("This book is already on the system!");
+		document.querySelector('#cadastrarBook').value = "Livro";
+	} else if(val !== undefined){
 
 		var tr = document.querySelector("#bookTable");
-		tr.innerHTML += "<tr>\n<td>" + BookID + "</td>\n<td>" + val + "</td>\n</tr>";
+		tr.innerHTML += "<tr>\n<td>" + bookID + "</td>\n<td>" + val + "</td>\n</tr>";
 		bookID += 1;
+		booklist.push(val);
+		document.querySelector('#cadastrarLivro').value = "Livro";
 	}
+}
+function cleanInputName(){
+	document.querySelector('#cadastrarNome').value = "";
+}
+function cleanInputBook(){
+	document.querySelector('#cadastrarLivro').value = "";
 }
