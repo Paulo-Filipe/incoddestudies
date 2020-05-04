35172ac0b4426e660266dfae4b291a7bd6915523
Paulo Filipe <63798027+Paulo-Filipe@users.noreply.github.com>
2020-05-04 13:18:02 -0300

    Update cardfinder.js

--- HS API/scripts/cardfinder.js
+++ HS API/scripts/cardfinder.js
@@ -1,10 +1,12 @@
 function findCard(){
+	document.querySelector('.showCards').innerHTML = "";
 	let val = document.querySelector('#cardSearch').value;
 	let showCard = document.querySelector('.showCards');
 
+
 	if (val.length >= 20){
 		alert("This name is too long!");
-		document.querySelector('#cadastrarNome').value = "";
+		document.querySelector('#cardSearch').value = "";
 	} else if((val !== undefined) && (val !== "")){
 		let search = `https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/${val}?collectible=1&locale=ptBR`
 		fetch(search, {
@@ -17,21 +19,22 @@ function findCard(){
 		.then(response => response.json())
   		.then(data => {
   			let cardList = [];
-  			let cardImgList = [];
+  			let cardIds = [];
   			
 
   			for (let i=0 ; i < data.length ; i++ ) {
   				cardList.push(data[i].name);
-  				cardImgList.push(data[i].img);
+  				cardIds.push(data[i].cardId);
   			};
   			
 
   			for (let i=0 ; i < cardList.length ; i++) {
   				showCard.innerHTML += `<h3>${cardList[i]}</h3>`;
-  				showCard.innerHTML += `<img src=${cardImgList[i]}>`;
+  				showCard.innerHTML += `<img src="https://art.hearthstonejson.com/v1/render/latest/ptBR/512x/${cardIds[i]}.png" >`;
   			};
   		})
-  		.catch(err =>{console.log(err);
+  		.catch(err =>{alert("No card found!");
   		});
+		document.querySelector('#cardSearch').value = "";
 	}
 }
