01f8eefea2da10d8ace39511f123a8aecdb23bf0
Paulo Filipe <63798027+Paulo-Filipe@users.noreply.github.com>
2020-05-08 14:15:12 -0300

    primeira etapa concluída

--- 2ndtask/scripts/2ndtask.js
+++ 2ndtask/scripts/2ndtask.js
@@ -1,18 +1,36 @@
 
 function getUsers(){
 
-  let show =  document.querySelector("showUsers");
+  let show =  document.querySelector('.showUsers');
   let search = `https://quizka-development.herokuapp.com/api/users`;
 
   fetch(search)
-  .then(response => response.json())
+  .then(response => {
+    if(!response.ok){
+        throw new Error("Not found!");
+      } else {return response.json()}
+  })
   .then(data => {return data.data})
-  .then(usersdata =>{
-    console.log(usersdata);
+  .then(usersData =>{
+    console.log(usersData);
 
     var idsList =   [];
     var usersList = [];
     var emailList = [];
-    
+
+    for(let i=0; i < usersData.length;i++){
+      idsList.push(usersData[i]._id);
+      usersList.push(usersData[i].username);
+      emailList.push(usersData[i].email);
+    }
+
+    for(let i=0; i < idsList.length; i++){
+      show.innerHTML +=`<tr>
+                          <td>${idsList[i]}</td>
+                          <td>${usersList[i]}</td>
+                          <td>${emailList[i]}</td>
+                        </tr>`;
+    }
   })
+  .catch(err =>{alert("something went wrong!")});
 }
